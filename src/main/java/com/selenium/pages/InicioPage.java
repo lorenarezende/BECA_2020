a94e063ff5b9244ded2cbeb09909f171bd2f3f75
unknown <lorennarezennde@gmail.com>
2020-06-12 01:10:37 -0300

    CT02

--- src/main/java/com/selenium/pages/InicioPage.java
+++ src/main/java/com/selenium/pages/InicioPage.java
@@ -2,38 +2,53 @@ package com.selenium.pages;
 
 import org.openqa.selenium.WebDriver;
 
+import com.selenium.core.Element;
 import com.selenium.uimaps.InicioMap;
 
 public class InicioPage {
 	WebDriver driver;
-	
+	Element teste;
+
 	public InicioPage(WebDriver driver) {
 		this.driver = driver;
 	}
+
 	InicioMap inicioMap = new InicioMap(driver);
-	
+
 	public void selecionarCidadeSaida() {
-		//San Diego
+		// San Diego
 		inicioMap.selectCidadeSaida.select(driver, "San Diego");
 	}
-	
+
 	public void selecionarDestino() {
-		//New York
+		// New York
 		inicioMap.selectDestino.select(driver, "New York");
 	}
-	
+
 	public void submeterFormulario() {
-		//Submit
+		// Submit
 		inicioMap.submeterForm.click(driver);
 	}
-	
-	public void selecionarTipoFormulario() {
-		inicioMap.selectTipoFormulario.select(driver, "Feedback");
-		
+
+	public void selecionarPrimeiraOpcao() {
+		// Clicar na primeira opção de vôo
+		inicioMap.selectPrimeiraOpcao.click(driver);
 	}
-	
-	
-	public void selecionarDemoParametro() {
-		inicioMap.selectDemoParameter.click(driver);
+
+	String lista2[] = { "Lorena", "Rua Teste, 148", "Uberlândia", "MG", "75740000", "8675734567", "09", "2020",
+			"LORENA TESTE" };
+
+	public void preencherFormulario() {
+		for (int i = 0; i < lista2.length; i++) {
+			// inicioMap.preencheFormulario.sendKey(driver, lista2[i]);
+			inicioMap.Teste(i).sendKey(driver, lista2[i]);
+
+		}
+		inicioMap.selectRemember.click(driver);
+
+	}
+
+	public void clicaPurchaseFlight() {
+		inicioMap.clickPurchaseFlight.click(driver);
 	}
 }
--- src/main/java/com/selenium/uimaps/InicioMap.java
+++ src/main/java/com/selenium/uimaps/InicioMap.java
@@ -4,20 +4,17 @@ import org.openqa.selenium.WebDriver;
 
 import com.selenium.core.Element;
 import com.selenium.core.enums.ByValue;
+import com.selenium.pages.InicioPage;
 
 public class InicioMap {
 	WebDriver driver;
+	
+	String lista1[] = {"//input[@name='inputName']","//input[@name='address']", "//input[@name='city']", "//input[@name='state']"};
 	
 	public InicioMap(WebDriver driver) {
 		this.driver = driver;
 	}
 	
-	////div[@class='nav']/select
-	public Element selectTipoFormulario = new Element(driver, ByValue.XPATH, "//div[@class='nav']/select");
-	
-	////a[@href='https://wpforms.com/demo/neighborhood-events-questionnaire-form-demo/']/
-	public Element selectDemoParameter = new Element (driver, ByValue.XPATH, "//a[@href='https://wpforms.com/demo/neighborhood-events-questionnaire-form-demo/']");
-
 	////div[@class='container']//select[@name='fromPort']
 	public Element selectCidadeSaida = new Element (driver, ByValue.XPATH, "//div[@class='container']//select[@name='fromPort']");
 	
@@ -26,4 +23,21 @@ public class InicioMap {
 	
 	////input[@value='Find Flights']
 	public Element submeterForm = new Element (driver, ByValue.XPATH, "//input[@value='Find Flights']");
+	
+	//input[@value=43]
+	public Element selectPrimeiraOpcao = new Element (driver, ByValue.XPATH, "//input[@value='Choose This Flight']");
+
+	//função que varre um vetor que contém xpath dos campos do formulário, que serão preenchidos com os itens da lista2
+	public Element Teste (int i) {
+	String lista1[] = {"//input[@name='inputName']","//input[@name='address']", "//input[@name='city']", "//input[@name='state']", "//input[@name='zipCode']", "//input[@name='creditCardNumber']", "//input[@name='creditCardMonth']", "//input[@name='creditCardYear']","//input[@name='nameOnCard']"};
+		Element preencheFormulario = new Element (driver, ByValue.XPATH,lista1[i]);
+		return preencheFormulario;
+	}
+	
+	////input[@name='rememberMe']
+	public Element selectRemember = new Element (driver, ByValue.XPATH,"//input[@name='rememberMe']");
+	
+	////input[@value='Purchase Flight']
+	public Element clickPurchaseFlight = new Element (driver, ByValue.XPATH,"//input[@value='Purchase Flight']");
+	
 }
